9bbd23eb6aeaad2695c60f73dbcfa90e0d1842bc
DerAlexmeister <alexweb199@hotmail.de>
2022-03-08 10:46:48 +0100

    (Added) minor changes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 DESCRIPTION = read('README.rst')
 setup(
     name='umap2',
-    version='2.0.2',
+    version='2.0.3',
     description='USB Host Security Assessment Tool - Revision 2',
     long_description=DESCRIPTION,
     author='NCCGroup & Cisco SAS team',
